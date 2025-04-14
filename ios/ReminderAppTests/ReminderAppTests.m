30b5959d0d442c72ee30c596c122f82a41f89274
Shashi <shashid321@gmail.com>
2025-04-14 11:28:38 +0530

    first commit

--- ios/ReminderAppTests/ReminderAppTests.m
+++ ios/ReminderAppTests/ReminderAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ReminderAppTests : XCTestCase
+@interface RxDozTests : XCTestCase
 
 @end
 
-@implementation ReminderAppTests
+@implementation RxDozTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
@@ -64,3 +64,4 @@
 }
 
 @end
+
